4292e6d7fe26ea9f32486189d724dc9c7d818d44
Adriano Harm <adhkr81@gmail.com>
2022-08-07 18:32:27 -0300

    post

--- index.js
+++ index.js
@@ -35,15 +35,10 @@ app.get('/api/notes', (request, response) => {
 
 //get by id
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
-  if(note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
   })
+})
 
   // delete
 app.delete("/api/notes/:id", (request, response) => {
@@ -62,25 +57,24 @@ const generateId = () => {
 }
 
 // add name
+
 app.post("/api/notes", (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({error: "content missing"})
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important || false, 
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
-
 
 
 
