20232244d5ab8dc6ffbbd5ddc54d18d2c74e045f
Adriano Harm <adhkr81@gmail.com>
2022-08-07 16:27:47 -0300

    dot env & mongoose models

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const Note = require('./models/note')
+
+
 
 //middleware
 const requestLogger = (request, response, next) => {
@@ -16,35 +20,17 @@ app.use(requestLogger)
 app.use(cors())
 app.use(express.static('build'))
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+
 
 
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>")
 })
 
-//get all
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes)
+  })
 })
 
 //get by id
@@ -100,9 +86,7 @@ app.post("/api/notes", (request, response) => {
 
 
 
-
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
