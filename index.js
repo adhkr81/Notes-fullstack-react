51692708bbd4c0eb2dbb60907f339eb153e50bad
Adriano Harm <adhkr81@gmail.com>
2022-08-08 12:38:20 -0300

    backend routes

--- index.js
+++ index.js
@@ -15,10 +15,23 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+
+
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
 app.use(express.static('build'))
+app.use(errorHandler)
 
 
 
@@ -34,30 +47,29 @@ app.get('/api/notes', (request, response) => {
 })
 
 //get by id
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
   // delete
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
 
-  response.status(204).end()
-})
 
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-// add name
-
+  //add a new record
 app.post("/api/notes", (request, response) => {
   const body = request.body
 
@@ -75,6 +87,22 @@ app.post("/api/notes", (request, response) => {
     response.json(savedNote)
   })
 })
+
+  //change importance
+app.put('/api/notes/:id', (request, response, next) => {
+   const body = request.body
+ 
+   const note = {
+     content: body.content,
+     important: body.important,
+   }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+     .then(updatedNote => {
+       response.json(updatedNote)
+     })
+     .catch(error => next(error))
+  })
 
 
 
